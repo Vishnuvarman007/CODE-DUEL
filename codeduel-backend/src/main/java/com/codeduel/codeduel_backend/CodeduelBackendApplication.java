c77d3b1a25d1d7129b28ec894ede3ca47b676f9f
Vishnuvarman007 <vishnuvarmanp@gmail.com>
2026-05-21 15:55:10 +0530

    base of the project

---
+++ codeduel-backend/src/main/java/com/codeduel/codeduel_backend/CodeduelBackendApplication.java
@@ -0,0 +1,14 @@
+package com.codeduel.codeduel_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeduelBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CodeduelBackendApplication.class, args);
+	}
+
+}
---
+++ codeduel-backend/src/test/java/com/codeduel/codeduel_backend/CodeduelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeduel.codeduel_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeduelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
