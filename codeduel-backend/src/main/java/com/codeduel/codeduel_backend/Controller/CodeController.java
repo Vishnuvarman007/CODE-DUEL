18685d193a61aa9c59eff9c724ae42a076b515d6
Vishnuvarman007 <vishnuvarmanp@gmail.com>
2026-05-29 21:46:07 +0530

    spring security added

--- codeduel-backend/src/main/java/com/codeduel/codeduel_backend/Controller/CodeController.java
+++ codeduel-backend/src/main/java/com/codeduel/codeduel_backend/Controller/CodeController.java
@@ -2,14 +2,16 @@ package com.codeduel.codeduel_backend.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/home")
 public class CodeController {
 
-    @GetMapping
-    public String home(){
-        return "welcome to code duel";
+
+    @GetMapping("/student")
+    public String fun(@RequestParam String name){
+        return "hi "+name;
     }
 }
